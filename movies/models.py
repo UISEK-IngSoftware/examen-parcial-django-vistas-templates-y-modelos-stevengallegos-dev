6c467954b2f2f6fdc88811b179e00ad5e95a67e9
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2025-11-15 23:53:57 -0500

    finalización examen P1

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)                
+    release_year = models.PositiveIntegerField()            
+    director_name = models.CharField(max_length=100)      
+    genre = models.CharField(max_length=50)                
+    synopsis = models.TextField()                          
+
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import Movie
 
-# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    return render(request,'index.html', {
+        'movies': movies
+        })
+
+def movie_details(request, movie_id):
+    movie = Movie.objects.get(id = movie_id)
+    return render(request, 'movie_details.html', {
+        'movie': movie
+    })
